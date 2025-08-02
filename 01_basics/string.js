a22e89d20a8cc7a4f8109c947cd849116c16dd8a
priya <pg0776281@gmail.com>
2025-08-03 00:40:20 +0530

    Create string.js
    .

---
+++ 01_basics/string.js
@@ -0,0 +1,5 @@
+let name="Priya";
+let sur_name="Gupta";
+console.log(`My name is ${name} and my surname is ${sur_name}.`);
+
+/////here we have learned about the technique to add the value without using any (+) concept, we can use `${variable_name}`.
